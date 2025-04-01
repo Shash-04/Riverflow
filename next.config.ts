daeb1ec6c2fef6316cd5380b25923e9f910e1496
Shash-04 <shashwatvaish2@gmail.com>
2025-04-01 23:18:09 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["aceternity.com","cloud.appwrite.io"], // Add the domain here
+  },
 };
 
 export default nextConfig;
