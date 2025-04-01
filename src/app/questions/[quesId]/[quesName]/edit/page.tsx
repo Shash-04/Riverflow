b69fdd719c020961b3141131bc1ec253b99e2926
Shash-04 <shashwatvaish2@gmail.com>
2025-04-01 23:44:24 +0530

    a

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -3,10 +3,17 @@ import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
 
-const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
+// Add this type definition to match Next.js's expectations
+type PageProps = {
+  params: { quesId: string; quesName: string };
+  searchParams?: Record<string, string | string[] | undefined>;
+};
 
-    return <EditQues question={question} />;
+// Use the correct type annotation
+const Page = async ({ params }: PageProps) => {
+  const question = await databases.getDocument(db, questionCollection, params.quesId);
+
+  return <EditQues question={question} />;
 };
 
 export default Page;
