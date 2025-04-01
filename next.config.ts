0d55e13e6b771842e00aefe0bad9c34645dfd3f0
Shash-04 <shashwatvaish2@gmail.com>
2025-04-01 23:35:26 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["aceternity.com","cloud.appwrite.io"], // Add the domain here
   },
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
